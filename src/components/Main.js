28d590f2171bd5ada3e304d74e25f93e2e0d0b6e
Ankush <ankushsagar30@gmail.com>
2022-08-27 22:56:28 +0530

    improved accessibility

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,12 +4,15 @@ import styles from './Main.module.css';
 
 export default function Main(){
   // const [factId, setFactId] = useState(-1);
+  const srText = <span className='sr-only'>Press Enter to get Next</span>
   const [fact, setFact] = useState("");
+  const [srButtonInfo, setSrButtonInfo] = useState(srText);
 
   const newFact = () => {
     axios.get("https://uselessfacts.jsph.pl/random.json?language=en")
       .then((response) => {
         setFact(response.data.text);
+        setSrButtonInfo(srText);
         // setFactId(response.data.id);
       })
       .catch((error) => {
@@ -23,9 +26,9 @@ export default function Main(){
   return (
     <>
       <div className={styles.container}>
-        <div className={styles.factContainer}>
-          <h1 className={styles.fact}>{fact}</h1>
-          <button onClick={newFact} className={styles.newButton}>New</button>
+        <div role='main' className={styles.factContainer}>
+          <h1 aria-live='polite' className={styles.fact}>{fact}{srButtonInfo}</h1>
+          <button autoFocus title='get next random fact' onClick={newFact} className={styles.newButton}>Next Random Fact</button>
         </div>
       </div>
     </>
