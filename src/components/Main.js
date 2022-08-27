3d756a00fd12594a05ca8f80ce226e3a8bb487ad
Ankush <ankushsagar30@gmail.com>
2022-08-27 23:08:37 +0530

    improved accessibility little more

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,15 +4,15 @@ import styles from './Main.module.css';
 
 export default function Main(){
   // const [factId, setFactId] = useState(-1);
-  const srText = <span className='sr-only'>Press Enter to get Next</span>
+  let srText = "Press Enter to get Next"
+  // let srElement= <span className='sr-only'></span>
   const [fact, setFact] = useState("");
-  const [srButtonInfo, setSrButtonInfo] = useState(srText);
+  // const [srButtonInfo, setSrButtonInfo] = useState(srText);
 
   const newFact = () => {
     axios.get("https://uselessfacts.jsph.pl/random.json?language=en")
       .then((response) => {
         setFact(response.data.text);
-        setSrButtonInfo(srText);
         // setFactId(response.data.id);
       })
       .catch((error) => {
@@ -22,13 +22,21 @@ export default function Main(){
 
   useEffect(() => {
     newFact();
-  }, [])
+  }, []);
+
+  useEffect(() => {
+    srText = "Press Enter to get Next"
+  }, [fact]);
+
   return (
     <>
       <div className={styles.container}>
         <div role='main' className={styles.factContainer}>
-          <h1 aria-live='polite' className={styles.fact}>{fact}{srButtonInfo}</h1>
-          <button autoFocus title='get next random fact' onClick={newFact} className={styles.newButton}>Next Random Fact</button>
+          <h1 aria-live='polite' className={styles.fact}>
+            {fact}
+            <span className='sr-only'>{srText}</span>
+            </h1>
+          <button type='button' autoFocus title='get next random fact' onClick={newFact} className={styles.newButton}>Next Random Fact</button>
         </div>
       </div>
     </>
